3dad07886d6657a1d7d4bf10dfa820ba3b0fce34
Sean Mauff <sjmauff@hotmail.com>
2016-04-11 16:41:25 +1000

    Made Health bars Made damage text Set layout for health on test scene

---
+++ TeamMimi/Assets/_Scripts/_UI/DamageText.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DamageText : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_text;
+    Canvas canvas;
+    // Use this for initialization
+    void Start()
+    {
+        canvas = gameObject.GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           CreateText( Input.mousePosition, Random.Range(0, 20).ToString());
+        }
+    }
+    public void CreateText(Vector3 a_position, string a_text)
+    {
+        Text tmp = Instantiate(m_text);
+        tmp.transform.SetParent(canvas.transform);
+        tmp.transform.localPosition = a_position - new Vector3(Screen.width / 2, Screen.height / 2);
+        tmp.text = a_text;
+    }
+}
---
+++ TeamMimi/Assets/_Scripts/_UI/TextHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextHandler : MonoBehaviour {
+    [SerializeField]
+    private float m_lifetime = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<Text>().color *= m_lifetime;
+        m_lifetime -= .1f * Time.deltaTime;
+        if (m_lifetime <= 0)
+            Destroy(gameObject);
+        gameObject.transform.Translate(new Vector3(0, 15 * Time.deltaTime, 0));
+	}
+}
